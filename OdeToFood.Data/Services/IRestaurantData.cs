6eb7cb6917b274143bc6598909b92b93fbb6afad
Sharique-dotnet <shariquedotnet@gmail.com>
2021-06-23 18:54:13 +0530

    Add project files.

---
+++ OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,18 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        Restaurant Get(int id);
+        IEnumerable<Restaurant> GetAll();
+        void Add(Restaurant restaurant);
+        void Update(Restaurant restaurant);
+        void Delete(int id);
+    }
+}
